9c25fe3e25a02148c337fc22b3b2e12aff48bf87
Nick <Nick.Yang@activenetwork.com>
2019-12-24 00:10:23 +0800

    Create the repo

---
+++ Crawler/EastCrawler.cs
@@ -0,0 +1,34 @@
+﻿using AngleSharp;
+using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crawler
+{
+    class EastCrawler
+    {
+        public StringBuilder data = new StringBuilder();
+
+        public async void GetData()
+        {
+
+            var config = Configuration.Default.WithDefaultLoader();
+            // Load the names of all The Big Bang Theory episodes from Wikipedia
+            var address = "https://en.wikipedia.org/wiki/List_of_The_Big_Bang_Theory_episodes";
+            // Asynchronously get the document in a new context using the configuration
+            var document = await BrowsingContext.New(config).OpenAsync(address);
+            // This CSS selector gets the desired content
+            var cellSelector = "tr.vevent td:nth-child(3)";
+            // Perform the query to get all cells with the content
+            var cells = document.QuerySelectorAll(cellSelector);
+            // We are only interested in the text - select it with LINQ
+            var titles = cells.Select(m => m.TextContent);
+
+            foreach (var item in titles)
+            {
+                data.Append(item);
+            }
+        }
+    }
+}
---
+++ Crawler/Program.cs
@@ -0,0 +1,43 @@
+﻿using AngleSharp;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Crawler
+{
+    class Program
+    {
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+            var config = Configuration.Default.WithDefaultLoader();
+
+            var address = "https://xa.ke.com/xiaoqu/qujiang1/";
+            // Asynchronously get the document in a new context using the configuration
+            var document = await BrowsingContext.New(config).OpenAsync(address);
+            // This CSS selector gets the desired content
+            var cellSelector = "div.title a.maidian-detail";
+            // Perform the query to get all cells with the content
+            var cells = document.QuerySelectorAll(cellSelector);
+
+            //var document = await BrowsingContext.New(config).OpenAsync(address);
+            // We are only interested in the text - select it with LINQ
+            List<string> priceList = new List<string>();
+            List<string> building = new List<string>();
+            List<string> unitList = new List<string>();
+
+            foreach (var cell in cells)
+            {
+                var url = cell.GetAttribute("href");
+                var documentChild = await BrowsingContext.New(config).OpenAsync(url);
+                var priceSelector = "div.xiaoquDescribe span.xiaoquUnitPrice";
+                var buildingNumSelector = "div.xiaoquDescribe div.xiaoquInfo div:nth-child(5) span.xiaoquInfoContent";
+                var unitsSelector = "div.xiaoquDescribe div.xiaoquInfo div:nth-child(6) span.xiaoquInfoContent";
+
+                var unitsCell = documentChild.QuerySelectorAll(unitsSelector);
+
+            }
+
+            //Console.WriteLine(titles);
+        }
+    }
+}
